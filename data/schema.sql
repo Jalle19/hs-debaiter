f54d793fbc93143b3ccfd812cabb83b35af05389
Sam Stenvall <sam.stenvall@nitor.com>
2024-07-30 13:03:57 +0300

    Remove ON UPDATE current_timestamp()

--- data/schema.sql
+++ data/schema.sql
@@ -4,20 +4,19 @@ DATABASE `hs_debaiter` /*!40100 COLLATE 'utf8mb4_general_ci' */;
 CREATE TABLE `articles`
 (
     `id`         INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `guid`       VARCHAR(16)  NOT NULL,
-    `title`      VARCHAR(255) NOT NULL,
-    `url`        VARCHAR(255) NOT NULL,
-    `image_url`  VARCHAR(255) NULL DEFAULT NULL,
-    `created_at` TIMESTAMP    NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `guid`       VARCHAR(16)  NOT NULL COLLATE 'utf8mb4_general_ci',
+    `title`      VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `url`        VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `image_url`  VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `created_at` TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
     `updated_at` TIMESTAMP NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `guid` (`guid`),
-    INDEX        `url` (`url`)
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `guid` (`guid`) USING BTREE,
+    INDEX        `url` (`url`) USING BTREE
 ) COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=300
+AUTO_INCREMENT=414
 ;
-
 
 CREATE TABLE `article_titles`
 (

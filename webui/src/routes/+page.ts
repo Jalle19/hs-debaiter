bc2fe8503dc615f2c8342c884aecaff3f869e271
Sam Stenvall <sam.stenvall@nitor.com>
2024-07-31 17:35:39 +0300

    Rename PUBLIC_BASE_URL to PUBLIC_API_BASE_URL

--- webui/src/routes/+page.ts
+++ webui/src/routes/+page.ts
@@ -1,13 +1,13 @@
 import type { PageLoad } from './$types';
-import { PUBLIC_BASE_URL } from '$env/static/public';
+import { PUBLIC_API_BASE_URL } from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch }) => {
   // Fetch articles
-  let response = await fetch(`${PUBLIC_BASE_URL}/articles`);
+  let response = await fetch(`${PUBLIC_API_BASE_URL}/articles`);
   const articles = await response.json();
 
   // Fetch frequently changed articles
-  response = await fetch(`${PUBLIC_BASE_URL}/articles/frequently-changed`);
+  response = await fetch(`${PUBLIC_API_BASE_URL}/articles/frequently-changed`);
   const frequentlyChangedArticles = await response.json();
 
   return { articles, frequentlyChangedArticles };
--- webui/src/routes/article/[guid]/+page.ts
+++ webui/src/routes/article/[guid]/+page.ts
@@ -1,8 +1,8 @@
 import type { PageLoad } from './$types';
-import { PUBLIC_BASE_URL } from '$env/static/public';
+import { PUBLIC_API_BASE_URL } from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch, params }) => {
-  const response = await fetch(`${PUBLIC_BASE_URL}/article/${params.guid}`);
+  const response = await fetch(`${PUBLIC_API_BASE_URL}/article/${params.guid}`);
   const article = await response.json();
 
   return { article };

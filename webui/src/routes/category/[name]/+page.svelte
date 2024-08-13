223f4e554fb8d3c1f86c574ea60e62c6e7ea3e21
Sam Stenvall <sam.stenvall@nitor.com>
2024-08-13 15:44:20 +0300

    Clarify that category page just shows recent articles

--- webui/src/routes/category/[name]/+page.svelte
+++ webui/src/routes/category/[name]/+page.svelte
@@ -6,13 +6,17 @@
 </script>
 
 <div class="pure-u-1-1 l-box">
-  <h2>Articles from category <i>{$page.params.name}</i></h2>
+  <h2>Recent articles from category <i>{$page.params.name}</i></h2>
 
-  <ul>
-    {#each data.categoryArticles as article}
-      <li>
-        <ArticleSummary {article}></ArticleSummary>
-      </li>
-    {/each}
-  </ul>
+  {#if data.categoryArticles.length > 0}
+    <ul>
+      {#each data.categoryArticles as article}
+        <li>
+          <ArticleSummary {article}></ArticleSummary>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No recent articles found</p>
+  {/if}
 </div>

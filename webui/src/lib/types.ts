ee90cd31f91033cff321c35261343a1933ff8bee
Sam Stenvall <sam.stenvall@nitor.com>
2024-08-13 15:33:43 +0300

    Expand Article type

--- webui/src/lib/types.ts
+++ webui/src/lib/types.ts
@@ -1,7 +1,16 @@
+type ArticleTitle = {
+  title: string;
+  created_at: Date;
+};
+
 export type Article = {
   guid: string;
+  created_at: Date;
   title: string;
   num_titles: number;
+  image_url?: string;
+  url: string;
+  article_titles: ArticleTitle[];
 };
 
 export type Category = {

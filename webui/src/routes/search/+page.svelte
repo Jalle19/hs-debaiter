d97f6c5736d6e5387b7c42306625d67d78525c1a
Sam Stenvall <sam.stenvall@nitor.com>
2026-01-17 13:21:39 +0200

    Add help text

--- webui/src/routes/search/+page.svelte
+++ webui/src/routes/search/+page.svelte
@@ -12,12 +12,39 @@
 </script>
 
 <div class="pure-u-1-1 l-box">
+  <h2>Search</h2>
+
+  <p>Remember reading some ragebait but now you can't find it? No problem!</p>
+  <p>You can use some basic tricks to help find what you're looking for:</p>
+  <ul>
+    <li>
+      <pre>erika vikman</pre>
+      will find articles containing either
+      <pre>erika</pre>
+      or
+      <pre>vikman</pre>
+    </li>
+    <li>
+      <pre>"erika vikman"</pre>
+      will find articles containing literally "erika vikman"
+    </li>
+    <li>
+      <pre>erika -vikman</pre>
+      will find articles about all the other Erikas
+    </li>
+  </ul>
+  <p>All the usual caveats about the computer not actually understand Finnish apply.</p>
+</div>
+
+<div class="pure-u-1-1 l-box">
   <SearchForm searchQuery={data.searchQuery}></SearchForm>
 </div>
 
 <div class="pure-u-1-1 l-box">
   {#if data.searchQuery}
-    <h2>Search results for <i>"{data.searchQuery}"</i></h2>
+    <h2>
+      Search results for <pre>{data.searchQuery}</pre>
+    </h2>
     {#if data.searchResults.length > 0}
       <ul>
         {#each data.searchResults as article}
@@ -31,3 +58,11 @@
     {/if}
   {/if}
 </div>
+
+<style>
+  pre {
+    display: inline-block;
+    margin: 0;
+    background-color: #ddd;
+  }
+</style>
